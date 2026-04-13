f48f42967d8d70d13fd1ebd8fd30f3658b85684f
Hisako <52304998+ImHisako@users.noreply.github.com>
2026-04-13 15:26:23 +0200

    Update self_updater.go

--- self_updater.go
+++ self_updater.go
@@ -46,7 +46,7 @@ func init() {
 }
 
 func GetInstallerDownloadLink() string {
-	const BaseUrl = "https://github.com/Equicord/Equilotl/releases/latest/download/"
+	const BaseUrl = "https://github.com/ImHisako/IllegalcordInstaller/releases"
 	switch runtime.GOOS {
 	case "windows":
 		filename := Ternary(buildinfo.UiType == buildinfo.UiTypeCli, "EquilotlCli.exe", "Equilotl.exe")
